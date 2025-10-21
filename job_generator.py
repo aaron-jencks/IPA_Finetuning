2c2f9293cfbc63943284582972ff4591318852ae
aaron-jencks <aaronjencks@aol.com>
2025-10-21 13:05:19 -0400

    made templates for job generation

--- job_generator.py
+++ job_generator.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 import pathlib
-
+from typing import Dict, Optional, Any
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,20 +10,21 @@ logger = logging.getLogger(__name__)
 def generate_job_script(
         template: pathlib.Path, output: pathlib.Path,
         name: str, args: str,
-        cpus: int = 16, gpus: int = 1, timeout: str = '03:00:00'
+        cpus: int = 16, gpus: int = 1, timeout: str = '03:00:00',
+        extra_args: Optional[Dict[str, Any]] = None
 ):
     logger.info(f'Generating job {name} from template {template}')
     with open(template, "r") as template_fp:
         template_body = template_fp.read()
 
     logger.info(f'Using cli args: "{args}"')
-    body = template_body.format(
-        job_name=name,
-        args=args,
-        cpus=cpus,
-        gpus=gpus,
-        timeout=timeout,
-    )
+    format_data = extra_args if extra_args is not None else {}
+    format_data['job_name'] = name
+    format_data['args'] = args
+    format_data['cpus'] = cpus
+    format_data['gpus'] = gpus
+    format_data['timeout'] = timeout
+    body = template_body.format(**format_data)
 
     with open(output, "w+") as output_fp:
         output_fp.write(body)
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     ap.add_argument(
         "-t", "--template",
         type=pathlib.Path,
-        default=pathlib.Path("./jobs/fine-tune-template.sh"),
+        default=pathlib.Path("jobs/templates/fine-tune-template.sh"),
         help="Path to the template file"
     )
     ap.add_argument("job_name", type=str, help="Name of the job")
---
+++ jobs/templates/grid-search-fine-tune-template.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name={job_name}
+#SBATCH --account=PAS2836
+#SBATCH --output=/fs/ess/PAS2836/ipa_gpt/jobs/logs/%x-%j.out
+#SBATCH --error=/fs/ess/PAS2836/ipa_gpt/jobs/logs/errors/%x-%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node={cpus}
+#SBATCH --time={timeout}
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --gpus-per-node={gpus}
+
+echo "===== [$(date)] JOB STARTED ====="
+
+# Load required modules
+module load miniconda3/24.1.2-py310 cuda/12.4.1
+conda init bash
+conda activate nanogpt_cu124  # TODO change this to your personal environment
+
+echo "Python: $(which python) ($(python --version))"
+
+# setup paths
+scratch_prefix="/fs/scratch/PAS2836/ipa_gpt"
+scratch_github_prefix="$scratch_prefix/github"
+scratch_hf_cache_prefix="$scratch_prefix/cache"
+mkdir -pv $scratch_github_prefix $scratch_hf_cache_prefix
+
+repo_name="IPA_Finetuning"
+repo_dir="$scratch_github_prefix/$repo_name"
+cd "$repo_dir"
+
+echo "===== [$(date)] RUNNING PYTHON SCRIPT ====="
+
+# Run the actual script
+TQDM_DISABLE=1 python finetuning-exp.py \
+  "$SLURM_JOB_ID" {args}
+
+echo "===== [$(date)] CLEANING UP ====="
+
+rm "$0" "{temp_config_name}"
+
+echo "===== [$(date)] JOB COMPLETED ====="
