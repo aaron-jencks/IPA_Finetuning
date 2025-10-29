80df8944a7dc0f73b6ff6cbcfd803974b0867550
aaron-jencks <aaronjencks@aol.com>
2025-10-29 08:17:48 -0400

    integrated model type

--- jobs/finetune-rus-pol-entail.sh
+++ jobs/finetune-rus-pol-entail.sh
@@ -52,6 +52,7 @@ echo "===== [$(date)] RUNNING PYTHON SCRIPT ====="
 # Run the actual script
 TQDM_DISABLE=1 python finetuning-exp.py \
   "$SLURM_JOB_ID" config/finetune-rus-pol.json "$config_file" \
-  --train-langs $train_lang --eval-langs $eval_lang --cpus 16
+  --train-langs $train_lang --eval-langs $eval_lang --model-type $model_type \
+  --cpus 16
 
 echo "===== [$(date)] JOB COMPLETED ====="
