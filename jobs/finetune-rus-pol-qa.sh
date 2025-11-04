f50dc248ed0b4499091afa925858214758045e67
aaron-jencks <aaronjencks@aol.com>
2025-11-04 08:06:24 -0500

    updated to use new model type configs

--- jobs/finetune-rus-pol-qa.sh
+++ jobs/finetune-rus-pol-qa.sh
@@ -50,7 +50,7 @@ echo "===== [$(date)] RUNNING PYTHON SCRIPT ====="
 
 # Run the actual script
 PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" python finetuning-exp-qa.py \
-  "$SLURM_JOB_ID" config/finetune-rus-pol.json config/finetune-rus-pol-qa.json \
+  "$SLURM_JOB_ID" "config/finetune-rus-pol.json" "config/finetune-rus-pol-qa-${model_type}.json" \
   --train-langs $train_lang --eval-langs $eval_lang --model-type $model_type \
   --training-eval-size $eval_samples \
   --cpus 16 --debug
